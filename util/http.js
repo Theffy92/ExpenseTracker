ec899859216b148368a3e06c084bfc42217c3adc
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-07-24 11:45:34 -0400

    Add post request without handling errors yet

--- util/http.js
+++ util/http.js
@@ -1,1 +1,9 @@
-const storeExpense = ()=>{};
+import axios from 'axios';
+
+export function storeExpense(expenseData) {
+    axios.post(
+        'https://learning-project-rn-default-rtdb.firebaseio.com/expenses.json',
+        expenseData
+    );
+};
+
